a9af9bbe0ff79bf4b05ba4c9c376ee59cfc2d9a1
wncassillo <cassilloneto@gmail.com>
2025-02-20 21:14:54 -0300

    6-6

--- 6_EXPRESS/6-url_parameters/index.js
+++ 6_EXPRESS/6-url_parameters/index.js
@@ -7,24 +7,17 @@ const dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
-
 const basePath = path.join(dirname, "templates");
 
-//using the same thing we build in 6-3, to show an example of middleware.
-//creating a little example middleware
-const checkAuth = function (req, res, next) {
-  req.authStatus = true; //simullating an auth result
+//Using the same project as 6-3, now to learn how to ger parameters from the url.
 
-  if (req.authStatus) {
-    console.log("Logged in, you may continue");
-    next();
-  } else {
-    console.log("You're not logged in, log in to cotinue.");
-    next();
-  }
-};
-app.use(checkAuth);
-//
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  //gets the 'id' from the params in the req
+  console.log(`Searching for user: ${id}`);
+  res.sendFile(`${basePath}/users.html`);
+  //simulates a search and a redirect to determined users page
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${basePath}/index.html`);
