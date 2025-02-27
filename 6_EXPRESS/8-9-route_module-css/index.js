8f54373e52e80c6f732f659c89bb43f34ba67e67
wncassillo <cassilloneto@gmail.com>
2025-02-27 00:31:29 -0300

    6-9

--- 6_EXPRESS/8-9-route_module-css/index.js
+++ 6_EXPRESS/8-9-route_module-css/index.js
@@ -9,9 +9,14 @@ const basePath = path.join(dirname, "templates");
 const app = express();
 const port = 3000;
 
-//Using the same project as 6-7, now to learn about routes modules.
-import { userRouter } from "./users/index.js";
+//Using the same project as 6-7, now to learn about routes modules(6-8) and to apply css using express(6-9)
+
+//Router
+import { userRouter } from "./users/index.js"; //Importing the router module
 const users = userRouter;
+
+//CSS
+app.use(express.static("public")); //static files
 
 app.use(
   express.urlencoded({
